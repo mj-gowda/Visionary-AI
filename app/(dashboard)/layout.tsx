9963d28a0435061219f32dd88b414646a49704bb
manoj-gowda <manojsurya5616@gmail.com>
2023-11-15 19:15:52 +0530

    Created pro-subscription modal
    - Pro modal opens when free trials are finished
    - Currently does not allow subscriptions

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,11 +4,12 @@ import { getApiLimitCount } from "@/lib/api-limit";
 
 const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
     /**
- * Must be passed from a server component (layout) to the client component.
- * This is because the action check is in the server.
- * This cannot be called from a client component directly.
- */
+     * Must be passed from a server component (layout) to the client component.
+     * This is because the action check is in the server.
+     * This cannot be called from a client component directly.
+     */
     const apiLimitCount = await getApiLimitCount();
+
     return (
         <div className="h-full relative">
             <div
