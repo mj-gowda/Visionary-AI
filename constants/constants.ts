8745442e5b9854ac8e32fd472f251e3524022b6c
manoj-gowda <manojsurya5616@gmail.com>
2023-11-16 14:14:41 +0530

    Fetch colours from constants instead of manually setting them
    - Added action to get object based on its name
    - Added description field on constant

--- constants/constants.ts
+++ constants/constants.ts
@@ -2,40 +2,54 @@ import { Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
 
 export const MAX_FREE_COUNTS = 3;
 
-export const tools = [
-    {
-        label: "Conversation",
-        icon: MessageSquare,
-        href: "/conversation",
-        color: "text-violet-500",
-        bgColor: "bg-violet-500/10",
-    },
-    {
-        label: "Music Generation",
-        icon: Music,
-        href: "/music",
-        color: "text-emerald-500",
-        bgColor: "bg-emerald-500/10",
-    },
-    {
-        label: "Image Generation",
-        icon: ImageIcon,
-        color: "text-pink-700",
-        bgColor: "bg-pink-700/10",
-        href: "/image",
-    },
-    {
-        label: "Video Generation",
-        icon: VideoIcon,
-        color: "text-orange-700",
-        bgColor: "bg-orange-700/10",
-        href: "/video",
-    },
-    {
-        label: "Code Generation",
-        icon: Code,
-        color: "text-green-700",
-        bgColor: "bg-green-700/10",
-        href: "/code",
-    },
+export type Tool = {
+  label: string;
+  description: string;
+  icon: any; // the actual type would depend on what your icon objects are
+  href: string;
+  color: string;
+  bgColor: string;
+};
+
+export const tools: Tool[] = [
+  {
+    label: "Conversation",
+    description: "Our most advanced conversation model",
+    icon: MessageSquare,
+    href: "/conversation",
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+  },
+  {
+    label: "Music Generation",
+    description: "Generate music from your prompt",
+    icon: Music,
+    href: "/music",
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+  },
+  {
+    label: "Image Generation",
+    description: "Turn your prompt into an image",
+    icon: ImageIcon,
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10",
+    href: "/image",
+  },
+  {
+    label: "Video Generation",
+    description: "Turn your prompt into video",
+    icon: VideoIcon,
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10",
+    href: "/video",
+  },
+  {
+    label: "Code Generation",
+    description: "Code generation from descriptions",
+    icon: Code,
+    color: "text-blue-700",
+    bgColor: "bg-blue-700/10",
+    href: "/code",
+  },
 ];
